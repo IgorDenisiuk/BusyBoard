eef122acd81fda81c9e017629c8763af8da03ab8
IgorDenisiuk <73896271+IgorDenisiuk@users.noreply.github.com>
2020-12-13 21:01:04 +0300

    Add RGB View

--- BusyBoard/ViewController.swift
+++ BusyBoard/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redButton: UISlider!{
+    didSet {
+        redButton.value = 0
+        redButton.thumbTintColor = .red
+        }
+    }
+    
+    @IBOutlet var greenButton: UISlider! {
+        didSet {
+            greenButton.value = 0
+            greenButton.thumbTintColor = .green
+        }
+    }
+    
+    @IBOutlet var blueButton: UISlider! {
+        didSet {
+        blueButton.value = 0
+        blueButton.thumbTintColor = .blue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.backgroundColor = .black
+      
     }
-
-
+    
+    
+    @IBAction func colorView(_ sender: UISlider) {
+        colorView.backgroundColor = UIColor(red: CGFloat(redButton.value) / 255, green: CGFloat(greenButton.value) / 255, blue: CGFloat(blueButton.value) / 255, alpha: 1.0)
+        }
 }
-
